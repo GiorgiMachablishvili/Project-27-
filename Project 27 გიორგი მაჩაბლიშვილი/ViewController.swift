8c8d843ef442fe82400b3270476e61b8bdc3b0ad
GiorgiMachablishvili <g.machablishvili@yahoo.com>
2024-06-06 19:01:41 +0400

    Initial Commit

---
+++ Project 27 გიორგი მაჩაბლიშვილი/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 27 გიორგი მაჩაბლიშვილი
+//
+//  Created by Gio's Mac on 06.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
